f8ae97510401d5ab4d5827df8c5cec8c231b9ed1
patshone96 <patrick_oliver_@hotmail.es>
2023-04-08 16:24:05 +0200

    Database implementation - Data dump (to be modified)

--- app/src/main/java/com/example/alacartapp/MainActivity.java
+++ app/src/main/java/com/example/alacartapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.alacartapp;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,21 +11,37 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    // PRUEBAS BBDD FIRESTORE
+
+    // Access a Cloud Firestore instance from your Activity
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+
 
     //Variables used for the RecyclerView
     ArrayList<Dish> dishes;
@@ -39,6 +57,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+/*
+        // Create a new user with a first and last name
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "Ada");
+        user.put("last", "Lovelace");
+        user.put("born", 1815);*/
+
+
+
+
 
         // RECYCLER
         Intent intent = new Intent(this, DishDetailedScreen.class);
@@ -47,6 +75,7 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new GridLayoutManager(this, 1));
 
         dishes = new ArrayList<>();
+
 
         ArrayList<String> ingredientes = new ArrayList<>();
 
@@ -61,6 +90,34 @@ public class MainActivity extends AppCompatActivity {
         dishes.add(dish);
         dishes.add(dish);
         dishes.add(dish);
+
+
+        // Add a new document with a generated ID
+        Map<String, Object> dishList = new HashMap<>();
+        dishList.put(dish.getName(), dish);
+
+
+        db.collection("dishes")
+                .add(dishList)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+
+
+
+
+
+
+
 
         AdapterDishes adapterDishes = new AdapterDishes(dishes);
 
